35691bb929c621533503ad8bce73577c5b292b04
Laurence Reeve Lextego <161852156+curlyreeve@users.noreply.github.com>
2025-09-01 14:40:50 +0100

    fix: remove hard coded ip

--- packages/config-svc-be/src/constants.ts
+++ packages/config-svc-be/src/constants.ts
@@ -8,5 +8,6 @@ export const DATABASE_USERNAME = process.env['DATABASE_USERNAME'];
 export const DATABASE_PASSWORD = process.env['DATABASE_PASSWORD'];
 export const DATABASE_NAME_TEST = process.env['DATABASE_NAME_TEST'];
 export const AUTH_URL = process.env['AUTH_URL'];
+export const AUTH_SERVICE_URL = process.env['AUTH_SERVICE_URL'];
 
 export const INSTANCE_ID = `config-svc_${uuidv4()}`;
